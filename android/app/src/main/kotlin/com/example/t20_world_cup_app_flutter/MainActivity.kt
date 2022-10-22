670a22b386720faccd46c9eed57fd12aff1d5815
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2022-10-22 19:48:42 +0500

    t20 world cup flutter  app setup

---
+++ android/app/src/main/kotlin/com/example/t20_world_cup_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.t20_world_cup_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:t20_world_cup_app_flutter/view/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
